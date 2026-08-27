a526ca51cb539dafe22ab291e1d7ae506cb6fc6b
alexsauter <156185544+alexsauter@users.noreply.github.com>
2026-08-27 09:27:46 +0200

    Update greeting message in hello.cpp

--- cpp-examples/00_hello.cpp
+++ cpp-examples/00_hello.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, see you on Canvas!" << std::endl;
+    std::cout << "Hello there, you see this on Canvas!" << std::endl;
     return 0;
 }
