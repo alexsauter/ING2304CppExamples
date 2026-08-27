e876ac9383a137ff0660330cebb1a8f6a32a3aea
alexsauter <156185544+alexsauter@users.noreply.github.com>
2026-08-27 14:39:17 +0200

    Add A01_PullUP.cpp for using internal pull-up resistor

---
+++ cpp-examples/A01_PullUP.cpp
@@ -0,0 +1,8 @@
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(2, INPUT_PULLUP); // Internal pull-up resistor enabled
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, digitalRead(2)); //Inverted to pull-down version
+}
