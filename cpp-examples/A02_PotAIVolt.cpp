a0d5c70879975b0f7068177e4cbe3f287eed293b
alexsauter <156185544+alexsauter@users.noreply.github.com>
2026-09-07 21:08:21 +0200

    Add potentiometer reading and voltage display
    Implement analog reading from potentiometer and output voltage.

---
+++ cpp-examples/A02_PotAIVolt.cpp
@@ -0,0 +1,18 @@
+int pot_pin = A0;
+int pot_ai = 0; //Potensiometer målt som analog input
+long pot_mV = 0; //Potensiometer spenning i milli Volt
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  pot_ai = analogRead(pot_pin);
+  pot_mV = map(pot_ai,0,1023,0,5000); //Kunne vært pot_mV = pot_ai*5000/1024;
+  Serial.print("Analog value: ");
+  Serial.println(pot_ai);
+  Serial.print("Voltage: ");
+  Serial.print(pot_mV);
+  Serial.println(" mV");
+  delay(1000);
+}
